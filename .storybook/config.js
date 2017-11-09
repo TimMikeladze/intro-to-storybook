86cb1675b60bf9484769389a3ebab3986f1ecda9
Tim Mikeladze <tim.mikeladze@gmail.com>
2017-11-08 23:05:21 -0800

    feat(addons): Added knobs, console, and host addons to Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,12 @@
 import Theme from './decorators/Theme';
 import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import '@storybook/addon-console';
 
 const req = require.context('../stories', true, /\.story\.js$/)
 
 addDecorator(Theme);
+addDecorator(withKnobs);
 
 const loadStories = () => req.keys().forEach((filename) => req(filename))
 
